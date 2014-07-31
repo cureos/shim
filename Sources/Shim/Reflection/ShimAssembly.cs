2478c3c87802fad4526c520f4dd276ef07edb6cc
Anders Gustafsson <anders@cureos.com>
2014-07-31 14:47:33 +0200

    Replaced all occurences of Shim.NET with CSShim

--- Sources/Shim/Reflection/ShimAssembly.cs
+++ Sources/Shim/Reflection/ShimAssembly.cs
@@ -3,20 +3,20 @@
  *  All rights reserved.
  *  http://www.cureos.com
  *
- *	This file is part of Shim.NET.
+ *	This file is part of CSShim.
  *
- *  Shim.NET is free software: you can redistribute it and/or modify
+ *  CSShim is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Shim.NET is distributed in the hope that it will be useful,
+ *  CSShim is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Shim.NET.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with CSShim.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace System.Reflection
