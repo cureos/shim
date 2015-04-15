b8807f4a31519eb179df795c4dfc0cb94c3c261d
Anders Gustafsson <anders@cureos.com>
2015-04-15 14:45:56 +0200

    Added extension method for WaitHandle.Close. Closes #21

---
+++ Sources/Shim/Threading/WaitHandleExtensions.cs
@@ -0,0 +1,45 @@
+﻿/*
+ *  Copyright (c) 2013-2015, Cureos AB.
+ *  All rights reserved.
+ *  http://www.cureos.com
+ *
+ *	This file is part of Shim.
+ *
+ *  Shim is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Shim is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with Shim. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace System.Threading
+{
+    /// <summary>
+    /// Shim complement for the <see cref="WaitHandle"/> class. <see cref="WaitHandle"/> instance methods that are not available in the 
+    /// PCL profile are here provided as equivalent extension methods.
+    /// </summary>
+    public static class WaitHandleExtensions
+    {
+        #region METHODS
+
+        /// <include file='../_Doc/mscorlib.xml' path='doc/members/member[@name="M:System.Threading.WaitHandle.Close"]/*' />
+        /// <param name="waitHandle">Wait handle to be closed.</param>
+        public static void Close(this WaitHandle waitHandle)
+        {
+#if PROFILE328
+            throw new PlatformNotSupportedException("PCL");
+#else
+            waitHandle.Close();
+#endif
+        }
+
+        #endregion
+    }
+}
