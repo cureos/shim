6edd7eb75852afab2f763cb322758e5a83005734
Anders Gustafsson <anders@cureos.com>
2016-03-01 12:00:38 +0100

    Implemented Clone extension method. Fixes #41

---
+++ Sources/Shim/ICloneableExtensions.cs
@@ -0,0 +1,52 @@
+﻿/*
+ *  Copyright (c) 2013-2015, Cureos AB.
+ *  All rights reserved.
+ *  http://www.cureos.com
+ *
+ *	This file is part of Shim.
+ *
+ *  Shim is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Shim is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with Shim. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Linq;
+using System.Reflection;
+
+namespace System
+{
+    /// <summary>
+    /// Shim complement for the <see cref="ICloneable"/> interface. Runtime support for performing <see cref="ICloneable.Clone"/> when <see cref="ICloneable"/>
+    /// is not explcitly implemented.
+    /// </summary>
+    public static class ICloneableExtensions
+    {
+        /// <include file='_Doc/mscorlib.xml' path='doc/members/member[@name="M:System.ICloneable.Clone"]/*' />
+        /// <param name="thisObject">Object to clone.</param>
+        public static object Clone<T>(this T thisObject)
+        {
+#if PCL
+            throw new PlatformNotSupportedException("PCL");
+#else
+#if DOTNET
+            if (typeof(T).GetInterfaces().Contains(typeof(ICloneable)))
+#else
+            if (typeof(T).GetTypeInfo().ImplementedInterfaces.Contains(typeof(ICloneable)))
+#endif
+            {
+                return ((ICloneable)thisObject).Clone();
+            }
+            throw new ArgumentException("Cannot clone specified object", "thisObject");
+#endif
+        }
+    }
+}
---
+++ UnitTests/Shim.Tests.Shared/ICloneableExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿/*
+ *  Copyright (c) 2013-2015, Cureos AB.
+ *  All rights reserved.
+ *  http://www.cureos.com
+ *
+ *	This file is part of Shim.
+ *
+ *  Shim is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Shim is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with Shim. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace System
+{
+    using Xunit;
+
+    public class ICloneableExtensionsTests
+    {
+        [Fact]
+        public void InstanceClone_Array_ShouldNotThrow()
+        {
+            var array = new[] { 1, 2, 3, 4, 5 };
+            var e = Record.Exception(() => array.Clone());
+            Assert.Null(e);
+        }
+    }
+}
