971cc5d81e5b9b333d8645659b3a2ba1eac42c36
Anders Gustafsson <anders@cureos.com>
2016-03-02 09:30:08 +0100

    Updated version number

--- Sources/Shim/Properties/AssemblyInfo.cs
+++ Sources/Shim/Properties/AssemblyInfo.cs
@@ -46,4 +46,4 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.60301")]
+[assembly: AssemblyFileVersion("2.0.0.60302")]
